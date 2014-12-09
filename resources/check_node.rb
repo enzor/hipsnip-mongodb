27b993a024d1ee2264b84195caa28a78587f8213
Enzo Rivello <vincenzo.rivello@gmail.com>
2014-12-09 10:41:41 +0000

    removing healthcheck as it seems broken as now

--- resources/check_node.rb
+++ resources/check_node.rb
@@ -1,7 +1,7 @@
-actions :run
-default_action :run
+#actions :run
+#default_action :run
 
-attribute :node_ip, :kind_of => String, :name_attribute => true
-attribute :port, :kind_of => Integer, :default => 27017
-attribute :admin_user, :kind_of => String, :default => ""
-attribute :admin_pass, :kind_of => String, :default => ""
+#attribute :node_ip, :kind_of => String, :name_attribute => true
+#attribute :port, :kind_of => Integer, :default => 27017
+#attribute :admin_user, :kind_of => String, :default => ""
+#attribute :admin_pass, :kind_of => String, :default => ""
